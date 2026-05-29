f961a6db0802cc235ab7cff067291a51aa1ce2b5
lin <1@163.com>
2026-05-29 20:44:05 +0800

    clear

--- Ball.cpp
+++ Ball.cpp
@@ -18,19 +18,7 @@ void Ball::Draw()
     DrawCircleV(position, radius, RED);
 }
 
-// void Ball::BounceEdge(int screenWidth, int screenHeight) {
-//     // 左右边界
-//     if (position.x - radius <= 0 || position.x + radius >= screenWidth) {
-//         speed.x *= -1;
-//     }
-//     // 上边界
-//     if (position.y - radius <= 0) {
-//         speed.y *= -1;
-//     }
-//     // 下边界暂时不处理
-// }
-
-// 修改后的 Ball::BounceEdge 函数
+//  Ball::BounceEdge 函数
 void Ball::BounceEdge(int screenWidth, int screenHeight)
 {
     // 左右边界
